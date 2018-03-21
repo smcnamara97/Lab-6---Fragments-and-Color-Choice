156c179b4f8c6c062748f60aacfcf1281ebdb95d
Sean McNamara <seanmcnamara97@gmail.com>
2018-03-20 20:47:06 -0400

    First commit

--- app/src/main/java/edu/temple/paletteactivity3/MainActivity.java
+++ app/src/main/java/edu/temple/paletteactivity3/MainActivity.java
@@ -3,8 +3,7 @@ package edu.temple.paletteactivity3;
 //Stuff to import
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.app.Fragment;
 
 /**
  * Created by seanmcnamara on 3/13/18.
@@ -14,6 +13,7 @@ import android.app.FragmentTransaction;
 public class MainActivity extends AppCompatActivity implements PaletteFragment.PaletteInterface{
     //Make a global CanvasFragment
     CanvasFragment canvas_frag;
+    PaletteFragment palette_frag;
     static final String[] color_array = {"Red", "Blue", "Yellow", "Green", "Gray"};
     //Boolean to check if we have to fragments
     boolean two_frags;
@@ -23,29 +23,36 @@ public class MainActivity extends AppCompatActivity implements PaletteFragment.P
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //We want to check if we have the canvas_fragment
+
         two_frags = (findViewById(R.id.canvas_fragment) != null);
-        //Make fragment manager
-        FragmentManager fm = getFragmentManager();
-        //We beging our transaction
-        FragmentTransaction ft = fm.beginTransaction();
-        //add our palette_fragment
-        ft.add(R.id.palette_fragment, new PaletteFragment());
-        //commit
-        ft.commit();
+        canvas_frag = new CanvasFragment();
+        palette_frag = new PaletteFragment();
+        if(two_frags){
+            addFragment(palette_frag, R.id.palette_fragment);
+            addFragment(canvas_frag, R.id.canvas_fragment);
+        }else{
+            addFragment(palette_frag, R.id.palette_fragment);
+        }
+
+
 
     }
 
+
+
     @Override
-    public void onColorChosen(int position) {
+    public void sendColor(String color) {
+
         if(two_frags) {
-            canvas_frag.setColor(color_array[position]);
+            canvas_frag.setColor(color);
         } else {
-            FragmentTransaction transaction = getFragmentManager().beginTransaction();
-            transaction.replace(R.id.palette_fragment, new CanvasFragment());
-            transaction.addToBackStack(null);
-            transaction.commit();
+            palette_frag.setColor(color);
         }
+
+    }
+
+    private void addFragment (Fragment fragment, int containerId) {
+        getFragmentManager().beginTransaction().replace(containerId, fragment).commit();
     }
 
 
--- app/src/main/java/edu/temple/paletteactivity3/PaletteFragment.java
+++ app/src/main/java/edu/temple/paletteactivity3/PaletteFragment.java
@@ -2,6 +2,7 @@ package edu.temple.paletteactivity3;
 
 //import stuff
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,9 +14,8 @@ import android.widget.GridView;
 
 public class PaletteFragment extends Fragment {
 
-
+    View layout;
     static final String[] color_array = {"Red", "Blue", "Yellow", "Green", "Gray"};
-    PaletteInterface message;
 
     public PaletteFragment() {
         //Need an empty constructer
@@ -31,15 +31,15 @@ public class PaletteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //Inflate the layout for this fragment
-        View layout = inflater.inflate(R.layout.fragment_palette, container, false);
+        layout = inflater.inflate(R.layout.fragment_palette, container, false);
         //Make a GridView and give it the layout of our grid view with id grid_view
         GridView grid_view = (GridView)layout.findViewById(R.id.grid_view);
         //Getting the resources
         Resources res = getResources();
         //get the string array from strings.xml and put i
-        String[] color_array = res.getStringArray(R.array.colors);
+        String[] myColors = res.getStringArray(R.array.colors);
         //Use the PaletteAdapter we made to make a PaletteAdapter object
-        final PaletteAdapter palette_adapter = new PaletteAdapter(getActivity(), color_array);
+        final PaletteAdapter palette_adapter = new PaletteAdapter(getActivity(), myColors);
         //Setting our adapter for grid_view to the palette_adapter
         grid_view.setAdapter(palette_adapter);
         //Have onItemClickListener so we can interact with fragments when "cells" are clicked
@@ -48,8 +48,7 @@ public class PaletteFragment extends Fragment {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //Set text_color to the color that we chose
                 String text_color = PaletteFragment.color_array[position];
-                //Send out the message
-                message.onColorChosen(position);
+                ((PaletteInterface) getActivity()).sendColor(text_color);
 
             }
         });
@@ -58,8 +57,13 @@ public class PaletteFragment extends Fragment {
 
 
     public interface PaletteInterface {
-        void onColorChosen(int position);
+        void sendColor (String color);
 
+    }
+
+    //Function to set the background color of the CanvasFragment to the color specified
+    public void setColor(String color) {
+        layout.setBackgroundColor(Color.parseColor(color));
     }
 
 }
